b6642e171845a08aee8784a6e8434a448637932e
colbyzg <colby.glass@gmail.com>
2014-09-05 05:28:09 -0400

    commit

--- rest_tools.py
+++ rest_tools.py
@@ -8,8 +8,8 @@ except ImportError:
 # Simple HTTP GET
 def rest_get(url, user="", passwd=""):
 	if len(user) > 1 and len(passwd) > 1:
-		auth = HTTPBasicAuth(user,passwd)
-		r = requests.get(url, auth=auth)
+		# auth = HTTPBasicAuth(user,passwd)
+		r = requests.get(url, auth=(user, passwd))
 	else:
 		r = requests.get(url)
 	return json.loads(r.content)
@@ -18,8 +18,8 @@ def rest_get(url, user="", passwd=""):
 def rest_put(url, payload, user="", passwd=""):
 	my_headers = {'Content-type': 'application/json'}
 	if len(user) > 1 and len(passwd) > 1:
-		auth = HTTPBasicAuth(user,passwd)
-		r = requests.put(url, data=json.dumps(payload), auth=auth, headers=my_headers)
+		# auth = HTTPBasicAuth(user,passwd)
+		r = requests.put(url, data=json.dumps(payload), auth=(user, passwd), headers=my_headers)
 	else:
 		r = requests.put(url, data=json.dumps(payload), headers=my_headers)
 	return json.loads(r.content)
